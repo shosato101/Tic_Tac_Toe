5f3669debcf16605517a26190cb16274f9aeeb09
s.sato <beetbood@gmail.com>
2021-01-21 22:33:29 +0900

    improve：コメント、変数名の改善。ウィンドウを常にトップに表示。
    最後のリファクタリングとして変数名とコメントの見直し。
    また、ゲームのウィンドウが他ウィンドウの下に隠れると面倒なため、
    常にトップに表示するように変更しました。

--- tictactoe.py
+++ tictactoe.py
@@ -1,46 +1,66 @@
 import PySimpleGUI as sg
 
+"""
+Tic Tac Toe !
+
+Parameters
+----------
+field : dict
+    9マスのフィールドに相当する。○×マークを格納して、勝敗判定をする。
+
+done : list
+    ボタンを無効化するためのリスト。
+    マークの上書き防止、およびゲーム終了後のマーク記入を無効化する。
+    また、len(done) > 9 でゲーム決着、len(done) < 9 でゲーム続行中となる。
+
+count : int
+    ゲームのターン数。○と×のターンや表示など、こちらの値で判断。
 
 
-marks = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8"}  #結果判定のため。
+"""
+
+
+
+field = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8"}  #結果判定のため。
 done = []  #選択済みボタンのキーを格納。
 count = 0  #ゲームのターン数。
 
 
 def check_result():
     #横のライン
-    if (marks[0] == marks[1] == marks[2] or
-        marks[3] == marks[4] == marks[5] or
-        marks[6] == marks[7] == marks[8] or
+    if (field[0] == field[1] == field[2] or
+        field[3] == field[4] == field[5] or
+        field[6] == field[7] == field[8] or
     #縦のライン
-        marks[0] == marks[3] == marks[6] or
-        marks[1] == marks[4] == marks[7] or
-        marks[2] == marks[5] == marks[8] or
+        field[0] == field[3] == field[6] or
+        field[1] == field[4] == field[7] or
+        field[2] == field[5] == field[8] or
     #斜めのライン
-        marks[0] == marks[4] == marks[8] or
-        marks[2] == marks[4] == marks[6]):
+        field[0] == field[4] == field[8] or
+        field[2] == field[4] == field[6]):
         if count % 2 == 0:
-            sg.popup("×' win!")
+            sg.popup("×' win!", keep_on_top=True)
             window["turn"].update("Finish!  ×'s win!")
             finish()
         else:
-            sg.popup("○'s win!")
+            sg.popup("○'s win!", keep_on_top=True)
             window["turn"].update("Finish!  ○'s win!")
             finish()
         
     elif len(done) == 9 and count == 9:
-        sg.popup("Draw")
+        sg.popup("Draw", keep_on_top=True)
         window["turn"].update("Draw....")
 
 def finish():
-    for i in range(9):
-        done.append(i)  #9マスのボタンを無効化するため。
+    for i in range(9):  #0-8までの9つの要素を追加で全て格納し、フィールドの全てのボタンを無効化する。
+        done.append(i)  #つまり決着後は必ずlen(done) > 9となる。(len(done)が9以下であればゲームは続行中となる)
+                        
     
 
 layout = [[sg.Text("○'s turn", key="turn", font=('Helvetica', 19), size=(15, 1)),
            sg.Button("Reset", key="reset", size=(5, 1), font=('Helvetica', 9))],
-          [sg.Button(key=0), sg.Button(key=1), sg.Button(key=2)],
-          [sg.Button(key=3), sg.Button(key=4), sg.Button(key=5)],
+          [sg.Button(key=0), sg.Button(key=1), sg.Button(key=2)],  #ボタンのキーはfield{}のキーに相当。
+          [sg.Button(key=3), sg.Button(key=4), sg.Button(key=5)],  #つまりボタンの配置が、そのままfield{}の座標。
           [sg.Button(key=6), sg.Button(key=7), sg.Button(key=8)],
           ]
 
@@ -48,30 +68,31 @@ window = sg.Window('Tic Tac Toe', layout,
                    default_button_element_size=(2, 1),
                    font=('Helvetica', 50),
                    auto_size_buttons=False,
-                   grab_anywhere=False)
+                   grab_anywhere=False,
+                   keep_on_top=True)
                     
 
-#イベントループしつつ、作業に応じてウィンドウの情報を更新する
+#イベントループしつつ、対話に応じてウィンドウの情報を更新する
 while True:
-    event, values = window.read()  # 情報を更新するため
+    event, values = window.read()  # 情報更新
  
-    if event == sg.WIN_CLOSED:  # タイトルバーの×ボタンで終了させるため
+    if event == sg.WIN_CLOSED:  # タイトルバーの×ボタンで終了
         break
     
-    if event in done:  #マークの上書き防止、およびゲーム終了後のマーク記入禁止のため。
+    if event in done:  #○×マークの上書き防止のため。
         continue
 
     elif event in list(range(9)):
         if count % 2 == 0:
-            marks[event] = "circle"
+            field[event] = "circle"
             window[event].update("○")
             done.append(event)
             count += 1
             check_result()
-            if len(done) < 9:
+            if len(done) < 9: #ゲームが続行しているかを確認する。
                 window["turn"].update("×'s turn")
         else:
-            marks[event] = "cross"
+            field[event] = "cross"
             window[event].update("×")
             done.append(event)
             count += 1
@@ -81,7 +102,7 @@ while True:
     
     if event == "reset":
         for i in range(9): 
-            marks[i] = i  #結果判定用の辞書をリセット。
+            field[i] = i  #結果判定用の辞書をリセット。
             window[i].update("")  #書き込んだ○×の表示を消す。
         done = []
         count = 0
